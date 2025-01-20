cf30a108b9dc559aec2800cee1343111e485f12c
Syamil Taqiyuddin <syamil_t_ayyasy_x@telkomsel.co.id>
2025-01-20 14:27:54 +0700

    Add config database

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/Ayyasy123/dibimbing-take-home-test/entity"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	// ambil variable dari .env
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	// buat connection
+	dsn := dbUser + ":" + dbPass + "@tcp(" + dbHost + ":" + dbPort + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database", err)
+	}
+
+	err = db.AutoMigrate(
+		&entity.User{},
+		&entity.Event{},
+		&entity.Ticket{},
+	)
+
+	if err != nil {
+		log.Fatal("Failed to migrate database", err)
+	}
+
+	DB = db
+	log.Println("Database connected and migrated successfully")
+}
