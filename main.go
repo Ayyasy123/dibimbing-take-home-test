9590a46bb11cc27dde3179d49cd61d63ce1d319c
Syamil Taqiyuddin <syamil_t_ayyasy_x@telkomsel.co.id>
2025-01-21 15:56:14 +0700

    fix: add routes in main.go

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/Ayyasy123/dibimbing-take-home-test/config"
+	"github.com/Ayyasy123/dibimbing-take-home-test/routes"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	config.ConnectDatabase()
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	routes.SetupUserRoutes(config.DB, r)
+	routes.SetupEventRoutes(config.DB, r)
+	routes.SetupTicketRoutes(config.DB, r)
+
+	log.Println("Server running on port 8080")
+
+	err := r.Run(":8080")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
