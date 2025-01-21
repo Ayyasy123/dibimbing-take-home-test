76554233f0286190b5ebcca0be522d310de7da13
Syamil Taqiyuddin <syamil_t_ayyasy_x@telkomsel.co.id>
2025-01-21 15:54:46 +0700

    feat: add routes.go to define API endpoints

---
+++ routes/routes.go
@@ -0,0 +1,56 @@
+package routes
+
+import (
+	"github.com/Ayyasy123/dibimbing-take-home-test/controller"
+	"github.com/Ayyasy123/dibimbing-take-home-test/repository"
+	"github.com/Ayyasy123/dibimbing-take-home-test/service"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupUserRoutes(db *gorm.DB, r *gin.Engine) {
+	userRepo := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepo)
+	userController := controller.NewUserController(userService)
+
+	r.POST("/register", userController.RegisterUser)
+	r.POST("/login", userController.LoginUser)
+
+	userRoutes := r.Group("/users")
+	{
+		userRoutes.GET("", userController.FindAllUsers)
+		userRoutes.GET("/:id", userController.FindUserByID)
+		userRoutes.PUT("/:id", userController.UpdateUser)
+		userRoutes.DELETE("/:id", userController.DeleteUser)
+	}
+}
+
+func SetupEventRoutes(db *gorm.DB, r *gin.Engine) {
+	eventRepo := repository.NewEventRepository(db)
+	eventService := service.NewEventService(eventRepo)
+	eventController := controller.NewEventController(eventService)
+
+	eventRoutes := r.Group("/events")
+	{
+		eventRoutes.POST("", eventController.CreateEvent)
+		eventRoutes.GET("", eventController.FindAllEvents)
+		eventRoutes.GET("/:id", eventController.FindEventByID)
+		eventRoutes.PUT("/:id", eventController.UpdateEvent)
+		eventRoutes.DELETE("/:id", eventController.DeleteEvent)
+	}
+}
+
+func SetupTicketRoutes(db *gorm.DB, r *gin.Engine) {
+	ticketRepo := repository.NewTicketRepository(db)
+	ticketService := service.NewTicketService(ticketRepo)
+	ticketController := controller.NewTicketController(ticketService)
+
+	ticketRoutes := r.Group("/tickets")
+	{
+		ticketRoutes.POST("", ticketController.CreateTicket)
+		ticketRoutes.GET("", ticketController.FindAllTickets)
+		ticketRoutes.GET("/:id", ticketController.FindTicketByID)
+		ticketRoutes.PUT("/:id", ticketController.UpdateTicket)
+		ticketRoutes.DELETE("/:id", ticketController.DeleteTicket)
+	}
+}
